2cb4511159d0d1ac6e3a1fb1cc7679f989cfdc89
jhordanmedinac <jhordanmedinac@gmail.com>
2026-07-18 16:39:24 -0500

    as

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: '', // TODO: URL del backend en producción cuando se despliegue
+  apiUrl: 'https://backendsaasalba-bebycudydhbdczbx.chilecentral-01.azurewebsites.net',
 };
